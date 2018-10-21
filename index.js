3f9c8255b14c9e2ae664ffcba89596272c41ef22
Oussama Mlouk <o.mlouk@hotmail.co.uk>
2018-10-21 23:14:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0){
+    return "Your shopping cart is empty."
+  }
+  else if (cart.length === 1){
+    return
+  }
 }
 
 function total() {
